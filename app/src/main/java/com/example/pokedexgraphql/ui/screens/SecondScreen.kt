a17105d692e337ae0bfb674147954cac3f4125ab
Raul Vieira <raulguil@gmail.com>
2022-08-19 16:26:49 -0400

    add pokemon description to SecondScreen.kt

--- app/src/main/java/com/example/pokedexgraphql/ui/screens/SecondScreen.kt
+++ app/src/main/java/com/example/pokedexgraphql/ui/screens/SecondScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 import com.example.pokedexgraphql.ui.screens.home.HomeScreenViewModel
 
@@ -33,14 +34,22 @@ fun SecondScreen(
                 color = Color.White,
                 textAlign = TextAlign.Center,
                 text = viewModel.pokemon.value.name ?: "",
-                modifier = Modifier.padding(10.dp)
+                modifier = Modifier.padding(top = 5.dp)
+            )
+
+            Text(
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                text = viewModel.pokemon.value.classification ?: "",
+                modifier = Modifier.padding(top = 5.dp, bottom = 5.dp),
+                fontSize = 10.sp
             )
 
             Card() {
                 AsyncImage(
                     model = viewModel.pokemon.value.image,
                     contentDescription = null,
-                    modifier = Modifier.padding(10.dp)
+                    modifier = Modifier.padding(5.dp)
                 )
             }
         }
