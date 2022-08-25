ed6c024542915c9667beb2fd4cca4845523cbc83
Raul Vieira <raulguil@gmail.com>
2022-08-24 22:08:22 -0400

    remove unused files and dependencies

--- app/src/main/java/com/example/pokedexgraphql/ui/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pokedexgraphql.ui.navigation
-
-sealed class Screen(val route: String) {
-    object Home: Screen(route = "home_screen")
-    object Second: Screen(route = "second_screen")
-    object Third: Screen(route = "third_screen")
-    object Fourth: Screen(route = "fourth_screen")
-}
